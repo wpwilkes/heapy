da7a45f1128db59586a4266d80a71101eae8a02a
Wesley Wilkes <wwilkes@rpagency.com>
2022-05-11 23:09:45 -0500

    fixed tests

--- test_heapy.py
+++ test_heapy.py
@@ -12,14 +12,14 @@ class TestHeapy(unittest.TestCase):
     def test_MaxHeap(self):
         mheap = heapy.MaxHeap()
         for i in range(5):
-            mheap.insert(i, None)
+            mheap.insert((i, None))
         self.assertTrue(mheap.remove()[0] == 4)
         self.assertTrue(mheap.remove()[0] == 3)
 
     def test_MinHeap(self):
         mheap = heapy.MinHeap()
         for i in range(5):
-            mheap.insert(i, None)
+            mheap.insert((i, None))
         self.assertTrue(mheap.remove()[0] == 0)
         self.assertTrue(mheap.remove()[0] == 1)
 
